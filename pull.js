a3c6e61a6ec4d0c90787b43038fc3b6512950f8d
Paulo Llanes <paulollanes@Paulos-MBP.fios-router.home>
2018-05-10 22:46:52 -0400

    match azure functions script requirements

--- pull.js
+++ pull.js
@@ -1,50 +1,48 @@
-const https = require("https");
+module.exports = function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+    const https = require("https");
 
-function sendCoinlistRequest() {
+    function sendCoinlistRequest() {
+        const req = https.request('https://api.coinmarketcap.com/v1/ticker/', getTicker).end();
+    }
 
-    const req = https.request('https://api.coinmarketcap.com/v1/ticker/', getTicker).end();
-}
+    function getTicker(res) {
+        res.setEncoding('utf8')
 
-function getTicker(res) {
-    //console.log('STATUS: ' + res.statusCode);
-    //console.log('HEADERS: ' + JSON.stringify(res.headers));
-    res.setEncoding('utf8')
+        var body= '';
+        res.on('data', function(data) {
+            body += data;
+        });
 
-    var body= '';
-    res.on('data', function(data) {
-        body += data;
-        //console.log('BODY: ' + data);
-    });
+        res.on('end', function() {
+            body = JSON.parse(body);
 
-    res.on('end', function() {
-        body = JSON.parse(body);
-        //console.log(body.Data);
+            var keys = [];
+            for(var k in body)
+            {
+                context.log(body[k]);
+                keys.push(k);
+            }
+        });
+    };
 
-        var keys = [];
-        for(var k in body)
-        {
-            console.log(body[k]);
-            keys.push(k);
-        }
-        //console.log(body[99]);
-    });
+    sendCoinlistRequest();
+
+    /*
+    name: 'ChainLink',
+    symbol: 'LINK',
+    rank: '100',
+    price_usd: '0.44332',
+    price_btc: '0.00005011',
+    '24h_volume_usd': '10231500.0',
+    market_cap_usd: '155162000.0',
+    available_supply: '350000000.0',
+    total_supply: '1000000000.0',
+    max_supply: null,
+    percent_change_1h: '-0.0',
+    percent_change_24h: '-11.53',
+    percent_change_7d: '-1.37',
+    last_updated: '1524712757'
+    */
+    context.done();
 };
-
-sendCoinlistRequest();
-
-/*
-name: 'ChainLink',
-symbol: 'LINK',
-rank: '100',
-price_usd: '0.44332',
-price_btc: '0.00005011',
-'24h_volume_usd': '10231500.0',
-market_cap_usd: '155162000.0',
-available_supply: '350000000.0',
-total_supply: '1000000000.0',
-max_supply: null,
-percent_change_1h: '-0.0',
-percent_change_24h: '-11.53',
-percent_change_7d: '-1.37',
-last_updated: '1524712757'
-*/
